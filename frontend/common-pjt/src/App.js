ee73080e5112f0d0e4765d3bea0164894e04b249
window8397 <window8397@naver.com>
2022-07-29 11:56:29 +0900

    [FE] Feat: MainHeader에서 포인트, 프로필업데이트 아이콘, 도움말아이콘 배치

--- frontend/common-pjt/src/App.js
+++ frontend/common-pjt/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import './App.css';
 import Login from './components/auth/Login';
 import Pledge from './components/auth/Pledge';
 import Signup from './components/auth/Signup';
--- frontend/common-pjt/src/components/main/MainHeader.js
+++ frontend/common-pjt/src/components/main/MainHeader.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import logoImg from '../../assets/logo.png'
 import Rating from '@mui/material/Rating';
-
+import { MdPets, MdAccountCircle, MdHelpOutline } from 'react-icons/md'
 
 const Header = styled.div`
     display: flex;
@@ -9,6 +9,7 @@ const Header = styled.div`
     margin-left: 2rem;
     margin-right: 2rem;
     height: 5rem;
+    font-family: Jua;
 `
 
 const Logo = styled.img`
@@ -25,17 +26,25 @@ const Nickname = styled.div`
     margin-right: 1rem;
 `
 
+const point = 100000;
+const pointShow = point.toLocaleString('ko-KR');
 
 
 const MainHeader = () => {
+
+
     return (
         <div>
             <Header>
                 <Logo src={logoImg}/>
                 
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <Nickname>뚜웅따앙</Nickname>
+                    <Nickname>닉네임</Nickname>
                     <Rating style={{ color: '#FFF672', marginRight: '1rem' }} size="large" value={3.5} precision={0.5} readOnly />
+                    <MdPets style={{ color: '#F38BA0', fontSize: '2rem', marginRight: '0.5rem' }}/>
+                    <p style={{ color: '#333333', fontSize: '1.5rem', marginRight: '1rem' }}>{pointShow}</p>
+                    <MdAccountCircle style={{ color: '#333333', fontSize: '2rem', marginRight: '0.5rem' }}/>
+                    <MdHelpOutline style={{ color: '#333333',fontSize: '2rem'}}/>
                 </div>
             </Header>
         </div>
