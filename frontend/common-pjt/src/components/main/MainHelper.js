ce28102e574625785e7f40cae9c7551036f8f635
window8397 <window8397@naver.com>
2022-07-30 22:06:03 +0900

    [FE] Update: Helper모달 위치 조정

--- frontend/common-pjt/src/components/main/MainHelper.js
+++ frontend/common-pjt/src/components/main/MainHelper.js
@@ -1,23 +1,12 @@
 import React, { useState } from "react"
 import styled from "styled-components"
 
-const ModalContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-`
-
 
 export const ModalBackdrop = styled.div`
-    width: 100%;
-    height: 100%;
-    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
     display: flex;
-    flex-flow: row wrap;
     justify-content: center;
     align-items: center;
     background-color: rgba(0, 0, 0, 0.5);
@@ -31,7 +20,7 @@ const ModalView = styled.div.attrs(props => ({
     padding: 30px 90px;
     background-color: white;
     border-radius: 30px;
-    color: #4000c7;
+    color: #333333;
 `
 
 
@@ -45,13 +34,11 @@ const MainHelper = ({openModalHelper}) => {
     }
 
     return (
-        <ModalContainer>
-                <ModalBackdrop onClick={closeModalProfile}>
-                    <ModalView>
-                        <div>Hello world</div>
-                    </ModalView>    
-                </ModalBackdrop>
-        </ModalContainer>
+        <ModalBackdrop onClick={closeModalProfile}>
+            <ModalView>
+                <div>Hello world</div>
+            </ModalView>    
+        </ModalBackdrop>
     )
 }
 
