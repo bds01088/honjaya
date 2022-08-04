35e0ef4d5a4bc2c31d3bc96e81eb8ab2aa4ab754
배송윤 <nename8@naver.com>
2022-08-04 14:11:30 +0900

    [FE] Add: 회원정보수정 컴포넌트 수정

--- frontend/common-pjt/src/components/main/profile/UpdateProfile.js
+++ frontend/common-pjt/src/components/main/profile/UpdateProfile.js
@@ -1,5 +1,77 @@
-const UpdateProfile = () => {
-    return <p>UpdateProfile</p>
-};
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../../../assets/logo.png'
 
-export default UpdateProfile;
+
+const Background = styled.div`
+  background-color: #fffdde;
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+`
+
+const Header = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Logo = styled.img`
+  /* position: fixed; */
+  /* display: inline; */
+  margin-left: 2rem;
+`
+
+const Container = styled.div`
+
+`
+
+const ImgBox = styled.div`
+
+`
+
+const FormBox = styled.form`
+
+`
+
+const LeftBox = styled.div`
+
+`
+const RightBox = styled.div`
+`
+
+const UpdateBtn = styled.div`
+
+`
+
+
+
+const UpdateProfile = () => {
+  return (
+
+    <Background>
+        <Header>
+            <Logo src={logo}></Logo>
+        </Header>
+
+        <Container>
+            <ImgBox>
+            </ImgBox>
+
+            <FormBox>
+                <LeftBox>
+                </LeftBox>
+
+                <RightBox>
+                </RightBox>
+
+                <UpdateBtn>
+                </UpdateBtn>
+            </FormBox>
+
+        </Container>
+    </Background>
+  
+  )
+}
+
+export default UpdateProfile
