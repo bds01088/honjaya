2f330f8cf85eff7ec379742f2fa43563f1bb5282
zsda3131 <zsda3131@naver.com>
2022-08-11 15:25:31 +0900

    [FE] Feat: 유저롤코드 보내기

--- frontend/common-pjt/src/components/meeting/UserVideoComponent.js
+++ frontend/common-pjt/src/components/meeting/UserVideoComponent.js
@@ -20,40 +20,42 @@ const Nickname = styled.div`
 export default class UserVideoComponent extends Component {
   getNicknameTag() {
     // Gets the nickName of the user
-    
-        
-    
+
     return JSON.parse(this.props.streamManager.stream.connection.data)
       .clientData
-    
-
   }
 
   getHashtags() {
-
-    const hashtags = JSON.parse(this.props.streamManager.stream.connection.data).hashtags
+    const hashtags = JSON.parse(
+      this.props.streamManager.stream.connection.data,
+    ).hashtags
     //배열반환
     // console.log("해시태그" , hashtags)
     return hashtags
-    
-  } 
+  }
 
-  
+  getRolecodes() {
+    const roleCodes = JSON.parse(
+      this.props.streamManager.stream.connection.data,
+    ).roleCodes
+    //배열반환
+    // console.log("해시태그" , hashtags)
+    return roleCodes
+  }
 
   render() {
     return (
       <div>
         {this.props.streamManager !== undefined ? (
           <StreamComponent className="streamcomponent">
-            <OpenViduVideoComponent streamManager={this.props.streamManager} 
-            />
+            <OpenViduVideoComponent streamManager={this.props.streamManager} />
             <Nickname>
               <p>{this.getNicknameTag()}</p>
               {/* Hashtags가 넘어올때 시간차가 생기면서 undefined 일때가 있음 이러한 오류를 방지해주기위해서
               &&를 이용해서 앞에가 참일때만 뒤를 수행하게 함 */}
-              {this.getHashtags() && this.getHashtags().map((item, idx) => (
-                  <h1># {item[1]}</h1>))}
-             
+              {this.getHashtags() &&
+                this.getHashtags().map((item, idx) => <h1># {item[1]}</h1>)}
+              <h1>{this.getRolecodes()}</h1>
             </Nickname>
           </StreamComponent>
         ) : null}
