c8045545917c4ef0324cc65f8b8ebbe173c122a7
songyunbae <baesongyun@gmail.com>
2022-08-13 13:19:15 +0900

    [FE] Fix: Merge 충돌 해결

--- frontend/common-pjt/src/components/meeting/UserVideoComponent.js
+++ frontend/common-pjt/src/components/meeting/UserVideoComponent.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import OpenViduVideoComponent from './OvVideo'
 import styled from 'styled-components'
 import { RiAlarmWarningFill } from 'react-icons/ri'
+import { connect } from 'react-redux'
+import { userReport } from './evaluate-slice'
+import axios from '../../api/http'
+
 
 const StreamDiv = styled.div`
   display: flex;
@@ -41,13 +45,24 @@ const Hashtag = styled.span`
 
 const RiAlarmWarning = styled(RiAlarmWarningFill)``
 
-export default class UserVideoComponent extends Component {
+class UserVideoComponent extends Component{
   constructor(props) {
     super(props)
     this.state = {
       isCommander: false,
+      myUserNo: undefined
     }
+    // this.userReport = this.userReoport.bind(this)
   }
+
+  componentDidMount() {
+    const { mode } = this.props
+    const userNo =  mode.user.userNo
+    this.setState({
+      myUserNo: userNo
+    })
+  }
+
 
   getNicknameTag() {
     // Gets the nickName of the user
@@ -62,6 +77,7 @@ export default class UserVideoComponent extends Component {
     ).hashtags
     //배열반환
     // console.log("해시태그" , hashtags)
+    console.log("담기기하나?",this.props.streamManager.stream.connection.data)
     return hashtags
   }
 
@@ -69,10 +85,41 @@ export default class UserVideoComponent extends Component {
     const roleCodes = JSON.parse(
       this.props.streamManager.stream.connection.data,
     ).roleCodes
-    //배열반환
-    // console.log("해시태그" , hashtags)
     return roleCodes
   }
+  
+  userReport() {
+    const { doUserReport } = this.props
+    console.log("")
+    const oppositeUserNo = JSON.parse(
+      this.props.streamManager.stream.connection.data,
+    ).userDatas.userNo
+    console.log("담기기하나?",this.props.streamManager.stream.connection.data)
+    console.log("유저넘버",oppositeUserNo)
+    const reportData = {
+      reportTo : oppositeUserNo,
+      reportType : 1,
+      reportMessage : "신고체크"
+    }
+    axios.get(`/honjaya/reports/${this.state.myUserNo}`)
+      .then((res) => {
+        if (res.data.trueOrFalse) {alert("중복신고금지")
+      } else 
+      { doUserReport(reportData)
+        .unwrap()
+        .then((res) => {
+          console.log("신고10번누적시응답", res)
+        })
+        .catch(err => {
+          console.log("신고 10번 누적시 에러응답", err)
+        })
+
+
+      }})
+
+    
+  }
+
 
   render() {
     return (
@@ -82,7 +129,9 @@ export default class UserVideoComponent extends Component {
             <OpenViduVideoComponent streamManager={this.props.streamManager} />
             <Profile>
               <Nickname>
-                {this.getNicknameTag()} <RiAlarmWarning></RiAlarmWarning>
+                {/* 화살표함수를 써주거나 바인드를 해준다.. 왜 화살표함수를 써야 에러가 안나지? 화살표 함수안쓰면 렌더링되면서 뜬금없이 신고함 */}
+                {this.getNicknameTag()} <RiAlarmWarning onClick={ () => {this.userReport()}}></RiAlarmWarning>
+
               </Nickname>
               {/* Hashtags가 넘어올때 시간차가 생기면서 undefined 일때가 있음 이러한 오류를 방지해주기위해서
               &&를 이용해서 앞에가 참일때만 뒤를 수행하게 함 */}
@@ -97,3 +146,17 @@ export default class UserVideoComponent extends Component {
     )
   }
 }
+
+
+const mapStateToProps = (state) => ({
+  mode: state.mode,
+  point: state.point
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    doUserReport: (data) => dispatch(userReport(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserVideoComponent)
