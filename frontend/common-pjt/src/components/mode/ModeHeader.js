595972428bc203130e27d58e44a7987f3eaaadcd
배송윤 <nename8@naver.com>
2022-08-16 17:48:42 +0900

    [FE] Feat: 모드선택에서 로고 누르면  메인페이지 이동

--- frontend/common-pjt/src/components/mode/ModeHeader.js
+++ frontend/common-pjt/src/components/mode/ModeHeader.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
   height: 10%;
-  margin-left: 5rem;
-  /* padding-left: 5rem; */
+  padding: 0 0 1rem 5rem;
   @media (max-width: 412px) {
     width: 100vw;
   }
 `
 
+const LogoLink = styled(Link)`
+  position: absolute;
+`
 const Logo = styled.img`
-  /* position: fixed; */
-  /* display: inline; */
   margin-left: 2rem;
+  height: 5.5rem;
 `
 
 const ModeHeader = () => {
   return (
-      <Header>
+    <Header>
+      <LogoLink to="/main" style={{ textDecoration: 'none' }}>
         <Logo src={logo}></Logo>
-      </Header>
+      </LogoLink>
+    </Header>
   )
 }
 
 export default ModeHeader
-
-
